51f8392b27c41b34065c950b272ee547230596f8
KAPPERASAIMOHITH <113877277+KAPPERASAIMOHITH@users.noreply.github.com>
2023-02-13 09:12:41 +0530

    Update 40scramblr string day4.10.py

--- 40scramblr string day4.10.py
+++ 40scramblr string day4.10.py
@@ -1,30 +1,20 @@
-month=input("enter a month")
-date=int(input("enter the date"))
-if(date>0 and date<=31):
-    if(month=='april' or month=='may'):
-         print("summer")
-    if(month=='june' and date<=20):
-         print("summer")
-    if(month=='march' and date>20):
-         print("summer")
-    if(month=='june' and date>20):
-         print("spring")
-    if(month=='july'or month=='august'):
-         print("spring")
-    if(month=='september' and date<=20):
-         print("spring")
-    if(month=='september' and date>20):
-         print("fall")
-    if(month=='october' or month=='november'):
-         print("fall")
-    if(month=='december' and date<=20):
-         print("fall")
-    if(month=='january' or month=='february'):
-         print("winter")
-    if(month=='march' and date<=20):
-         print("winter")
-    if(month=='december' and date>20):
-        print("winter")
-
-else:
-    print("enter valid input")
+def isScramble(s1, s2):
+    if len(s1) != len(s2) or sorted(s1) != sorted(s2):
+        return False
+    n = len(s1)
+    dp = [[[False for _ in range(n+1)] for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            dp[i][j][1] = s1[i] == s2[j]
+    for l in range(2, n+1):
+        for i in range(n-l+1):
+            for j in range(n-l+1):
+                for k in range(1, l):
+                    if (dp[i][j][k] and dp[i+k][j+k][l-k] or
+                        dp[i][j+l-k][k] and dp[i+k][j][l-k]):
+                        dp[i][j][l] = True
+                        break
+    return dp[0][0][n]
+s1=input("enter the string:")
+s2=input("enter the string:")
+print(isscramble(s1,s2))
